f0d82433e176e02fc7ceb251fdd72aa0d50b1b66
hiwotbible <hiwotbible@gmail.com>
2018-05-17 19:30:21 +0100

    complit

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,24 @@
-// Write code here to communicate with Github
+fetch("https://api.github.com/users/hiwotbible/repos")
+  .then(function (data) {
+    // Necessary line to parse the JSON (we'll cover this
+    // in more detail in upcoming weeks)
+    // Returns another Promisegithu
+    return data.json();
+  })
+  .then(function (repos) {
+    // 1. we gate the list
+    var reposList = document.querySelector("#repos-list");
+    repos.forEach(function (repo) {
+      // 2. we create the list item
+      var listItem = document.createElement("li");
 
+      // 3. we put the repo name to list item
+      listItem.appendChild(document.createTextNode(repo.name))
+      console.log(repo.name)
+      //4.put  list item in to list
+      reposList.appendChild(listItem)
+
+    });
+  });
+
+// Do something with the json response object"")
